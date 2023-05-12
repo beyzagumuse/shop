f980343af77dd595e8a4cd2851fccf081f6e80cb
ebeyzagumus <79407605+ebeyzagumus@users.noreply.github.com>
2023-05-12 11:49:57 +0300

    Start with Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { products } from './product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shop';
+  products = products;
 }
