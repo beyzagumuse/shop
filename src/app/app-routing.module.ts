030abcadf3f45540da2b5255cd159c2f856ec2e2
ebeyzagumus <79407605+ebeyzagumus@users.noreply.github.com>
2023-05-12 15:09:01 +0300

    S2 - 02
    Navigation bar eklendi. ps. container-fluid tüm ekran boyutuna orantılıyor.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,14 @@
 // Projeyi oluştururken routing e tamam demediğimiz için manuel olarak ekliyoruz.
+
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+
+const routes : Routes = [];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
