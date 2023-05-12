ba2ca24bcc1ba53c6181f27a83b80de1796dcb1e
ebeyzagumus <79407605+ebeyzagumus@users.noreply.github.com>
2023-05-12 15:50:32 +0300

    S2 - 04
    Yeni sayfalar oluşturuldu ve ürünler ile sepet onlara taşındı. Önemli bir kısım!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@
 
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ProductsComponent } from "./products/products.component";
+import { OrdersComponent } from "./orders/orders.component";
 
-
-const routes : Routes = [];
+const routes : Routes = [
+    {path : '', redirectTo : 'products', pathMatch : 'full'},
+    //{sayfanın linki,bu linke gidilince hangi component çalışacak}
+    {path : 'products', component : ProductsComponent},
+    {path : 'orders', component : OrdersComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
